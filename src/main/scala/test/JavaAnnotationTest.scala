55dc3f4bf65e6595d5e3cd403110fce99517b1a8
PJ Fanning <pjfanning@users.noreply.github.com>
2021-02-20 19:23:40 +0000

    Update JavaAnnotationTest.scala

--- src/main/scala/test/JavaAnnotationTest.scala
+++ src/main/scala/test/JavaAnnotationTest.scala
@@ -1,18 +1,14 @@
 package test
 
-import com.fasterxml.jackson.annotation._
-
-case class JacksonAnnotationTestCaseClass(@JsonProperty("foo") oof: String, bar: String)
+case class TestCaseClass(oof: String, bar: String)
 
 object JavaAnnotationTest extends App {
-  val cls = classOf[JacksonAnnotationTestCaseClass]
+
+  val cls = classOf[TestCaseClass]
 
   val constructor = cls.getConstructors.head
 
   val param1 = constructor.getParameters.head
 
   println(s"constructor params ${constructor.getParameters.map(_.getName).toSeq}")
-
-  //val annotations = param1.getAnnotations.toSeq
-  //println(s"annotations $annotations")
 }
